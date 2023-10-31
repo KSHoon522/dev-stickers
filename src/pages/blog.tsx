669de80a3bb53aa3e10b83e6b95d6099f51d2aad
Sang Hoon Kim <KSHoon522@gamil.com>
2023-10-31 11:25:21 +0900

    Layouts

---
+++ src/pages/blog.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+
+function Blog() {
+  return (
+    <Layout title="Blog">
+      <p>The most recent news on my shop</p>
+    </Layout>
+  );
+}
+export default Blog;
+
+export const Head = () => <Seo title="Blog" />;
