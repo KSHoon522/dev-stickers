6a2463ee189c181ac25c00b3edca7830b18db3c1
Sang Hoon Kim <KSHoon522@gamil.com>
2023-10-31 15:55:44 +0900

    graphQL

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,43 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import { PageProps, graphql } from "gatsby";
 
-function Blog() {
+function Blog({ data }: PageProps<Queries.BlogPostsQuery>) {
   return (
     <Layout title="Blog">
-      <p>The most recent news on my shop</p>
+      <section>
+        {data.allMdx.nodes.map((file, index) => (
+          <article key={index}>
+            <h3>{file.frontmatter?.title}</h3>
+            <h5>
+              {file.frontmatter?.author} in {file.frontmatter?.category}
+            </h5>
+            <h6>{file.frontmatter?.date}</h6>
+            <hr />
+            <p>{file.excerpt}</p>
+          </article>
+        ))}
+      </section>
     </Layout>
   );
 }
 export default Blog;
+
+export const query = graphql`
+  query BlogPosts {
+    allMdx {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "YYYY.MM.DD")
+          category
+          author
+        }
+        excerpt(pruneLength: 50)
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
