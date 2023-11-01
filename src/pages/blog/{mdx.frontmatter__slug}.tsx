41ff20067f24c95eb3a214a37048493f8a3de81e
Sang Hoon Kim <KSHoon522@gamil.com>
2023-11-01 12:48:28 +0900

    static/dynamic Images

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { graphql } from "gatsby";
 import Seo from "../../components/Seo";
-
+import { getImage, GatsbyImage } from "gatsby-plugin-image";
 interface IBlogPostProps {
   data: Queries.PostDetailQuery;
   children: any;
 }
 
 export default function BlogPost({ data, children }: IBlogPostProps) {
+  const image = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!
+  );
   return (
     <Layout title="">
+      <GatsbyImage image={image as any} alt={data.mdx?.frontmatter?.title!} />
       <div>{children}</div>
     </Layout>
   );
@@ -26,6 +30,11 @@ export const query = graphql`
         date
         title
         slug
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 450, placeholder: BLURRED)
+          }
+        }
       }
     }
   }
