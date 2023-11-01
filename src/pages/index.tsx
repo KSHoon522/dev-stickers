a33a367727b6e4cf2ccfcafb3be5c388df862b72
Sang Hoon Kim <KSHoon522@gamil.com>
2023-11-01 13:51:08 +0900

    Contentful

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 import { StaticImage } from "gatsby-plugin-image";
+import { PageProps, graphql, Link } from "gatsby";
+import { getImage, GatsbyImage } from "gatsby-plugin-image";
 
-const IndexPage = () => {
+const IndexPage = ({ data }: PageProps<Queries.StickersQuery>) => {
   return (
     <Layout title="Welcome to DevStickers 🖐🏻">
       <StaticImage
@@ -11,10 +13,37 @@ const IndexPage = () => {
         alt="Stickers on the wall
       "
       />
+      {data.allContentfulStickerPack.nodes.map((sticker) => (
+        <article>
+          <Link to={`/products/${sticker.id}`}>
+            <GatsbyImage
+              image={getImage(sticker.preview?.gatsbyImageData!)!}
+              alt={sticker.name!}
+            />
+            <h2>{sticker.name}</h2>
+            <h4>{sticker.price}</h4>
+          </Link>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export default IndexPage;
+
+export const query = graphql`
+  query Stickers {
+    allContentfulStickerPack {
+      nodes {
+        id
+        name
+        price
+        preview {
+          gatsbyImageData(placeholder: BLURRED, height: 250)
+        }
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Home" />;
